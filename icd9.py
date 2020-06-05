661d98c2c4034516deb0cc287b6630e495841cbd
vccheng2001 <vccheng@andrew.cmu.edu>
2020-06-04 17:17:46 -0700

    icd9 updated

--- icd9.py
+++ icd9.py
@@ -30,14 +30,33 @@ def main():
 
     #graph out
 
-    x = list(icd9_dict.keys())
-    length_dict = {key: len(value) for key, value in icd9_dict.items()}
-    y = list(length_dict.values())
-    plt.bar(x,y, color='purple',width = 0.5)
-    plt.xticks(x, rotation='vertical')
-    plt.ylim(top=2500)
-    plt.show()
-    print(icd9_dict)
+    #get_disease_name()
+    icd9_dict_sorted= {k: icd9_dict[k] for k in sorted(icd9_dict)}
+    for key, value in icd9_dict_sorted.items():
+        print(f"{key}: {(value)}")
+    #
+    # x = list(icd9_dict_sorted.keys())
+    # length_dict = {key: len(value) for key, value in icd9_dict_sorted.items()}
+    # y = list(length_dict.values())
+    # plt.bar(x,y, color='purple',width = 0.5)
+    # plt.xticks(x, rotation='vertical')
+    # plt.ylim(top=2500)
+    # plt.show()
+
+
+icd9_file1 = "icd9/D_ICD_DIAGNOSES.csv"
+def get_disease_name():
+    raw_csv = open(icd9_file1)
+    reader = csv.reader(raw_csv, delimiter=',')
+    for line in reader:
+        icd9_code = line[1]
+        name = line[2]
+        try:
+            icd9_dict[name] = icd9_dict.pop(icd9_code)
+        except:
+            continue
+
+
 
 scores_dict = {}
 score_file = "detailed_rankings.txt"
@@ -53,7 +72,7 @@ def scores():
 
 
 def get_score(subject_id):
-   return float(scores_dict[subject_id])
+    return float(scores_dict[subject_id])
 
 
 
